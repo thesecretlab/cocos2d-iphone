2a79de5753749ddfd54f6b70e4e8cc53db39c41b
Jon Manning <jon@secretlab.com.au>
2013-06-26 11:41:15 +1000

    Work around a flickring particle system bug (see http://stackoverflow.com/questions/15327277/sprite-flickering-artifacts-after-cocos2d-version-upgrade)

--- cocos2d/CCParticleSystemQuad.m
+++ cocos2d/CCParticleSystemQuad.m
@@ -419,13 +419,13 @@
 //	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(_quads[0])*_particleCount, _quads);
 	
 	// Option 2: Data
-	glBufferData(GL_ARRAY_BUFFER, sizeof(_quads[0]) * _particleCount, _quads, GL_DYNAMIC_DRAW);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(_quads[0]) * _particleCount, _quads, GL_DYNAMIC_DRAW);
 	
 	// Option 3: Orphaning + glMapBuffer
-//	glBufferData(GL_ARRAY_BUFFER, sizeof(_quads[0])*_totalParticles, nil, GL_STREAM_DRAW);
-//	void *buf = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-//	memcpy(buf, _quads, sizeof(_quads[0])*_particleCount);
-//	glUnmapBuffer(GL_ARRAY_BUFFER);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(_quads[0])*_totalParticles, nil, GL_STREAM_DRAW);
+	void *buf = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+	memcpy(buf, _quads, sizeof(_quads[0])*_particleCount);
+	glUnmapBuffer(GL_ARRAY_BUFFER);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
